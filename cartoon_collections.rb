9c4cc22a004220ff12a926d69edcdd2057771de6
Trevor Jameson <tjameson15@gmail.com>
2018-07-13 15:45:15 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,48 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index {|dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteers.collect! do |power|
+    "#{power.capitalize}!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.each {|call|
+    if call.size > 4
+      return true
+    end
+  }
+  return false
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheeses)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.each do |cheese|
+    cheese_types.each do |type|
+      if cheese === type
+        return cheese
+      end
+    end
+  end
+  return nil
 end
+
+#def find_the_cheese_include(cheeses)
+#  cheese_types = ["cheddar", "gouda", "camembert"]
+#  cheeses.each do |cheese|
+#    if cheese_types.include?(cheese)
+#      return cheese
+#    end
+#  end
+#  return nil
+#end
+
+# Test dangerous method "collect!" in #summon_captain_planet
+# planet = ["earth", "wind", "fire", "water", "heart"]
+# summon_captain_planet(planet)
+# puts planet
